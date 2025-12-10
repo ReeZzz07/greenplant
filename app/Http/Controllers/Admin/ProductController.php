ba8c6dad0da5ad0016fea19269676a2f0f917751
ReeZzz07 <130918228+ReeZzz07@users.noreply.github.com>
2025-12-10 10:23:50 +0300

    Добавлена явная конвертация чекбоксов is_active и is_featured в boolean для надежной работы на production

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -90,6 +90,10 @@ class ProductController extends Controller
 
         $validated['characteristics'] = $this->formatCharacteristics($request->input('characteristics', []));
 
+        // Явная конвертация чекбоксов в boolean для надежности на production
+        $validated['is_active'] = (bool)($validated['is_active'] ?? false);
+        $validated['is_featured'] = (bool)($validated['is_featured'] ?? false);
+
         Product::create($validated);
 
         return redirect()->route('admin.products.index')
@@ -201,6 +205,10 @@ class ProductController extends Controller
         unset($validated['gallery_images'], $validated['remove_images']);
 
         $validated['characteristics'] = $this->formatCharacteristics($request->input('characteristics', []));
+
+        // Явная конвертация чекбоксов в boolean для надежности на production
+        $validated['is_active'] = (bool)($validated['is_active'] ?? false);
+        $validated['is_featured'] = (bool)($validated['is_featured'] ?? false);
 
         $product->update($validated);
 
